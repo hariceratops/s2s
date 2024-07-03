d0035779c7d17e463aafd4983badb931d063b6a8
hari <hariprasad12994@gmail.com>
2024-07-03 13:20:34 +0200

    Fixing array size for fixed_string buffer

--- include/fixed_string.hpp
+++ include/fixed_string.hpp
@@ -5,19 +5,25 @@
 #include <cstddef>
 #include <algorithm>
 
-// FixedString implementation
+
 template <std::size_t N>
 struct fixed_string {
-  std::array<char, N> value;
+  std::array<char, N + 1> value;
   constexpr fixed_string(): value{} {};
   constexpr fixed_string(const char (&str)[N + 1]) {
-    std::copy_n(str, N, value.data());
+    std::copy_n(str, N + 1, value.data());
   }
   constexpr const char* data() const { return value.data(); }
   constexpr char* data() { return value.data(); }
+  constexpr auto size() const { return N; }
 };
 
 template <std::size_t N>
 fixed_string(const char (&)[N]) -> fixed_string<N - 1>;
+
+namespace static_test {
+static_assert(fixed_string("hello").size() == 5);
+}
+
 
 #endif // _FIXED_STRING_HPP_
