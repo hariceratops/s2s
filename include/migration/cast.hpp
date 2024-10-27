8f43b2554ed51a4f391380d155946ff81b4a2e21
hari <hariprasad12994@gmail.com>
2024-10-27 14:32:22 +0100

    Migrating cast function

--- include/migration/cast.hpp
+++ include/migration/cast.hpp
@@ -1,7 +1,9 @@
 #ifndef _CAST_HPP_
 #define _CAST_HPP_
 
-#include "field_meta.hpp"
+#include "field_reader.hpp"
+#include "../field_meta.hpp"
+#include "sc_type_traits.hpp"
 
 template <typename T>
 struct is_field;
@@ -45,24 +47,68 @@ struct is_struct_field_list<struct_field_list<Entries...>> : std::true_type {};
 template <typename T>
 constexpr bool is_struct_field_list_v = is_struct_field_list<T>::value;
 
+template <typename... expected_types>
+auto is_any_error(const expected_types&... expected_list) {
+  return (expected_list && ...);
+}
+
+// todo constraints
+template <typename expected_struct_field_list, typename error>
+auto operator|(const std::expected<expected_struct_field_list, error>& lhs, auto functor) {
+  if(lhs) return functor(*lhs); else return lhs;
+}
+
+// todo constraints
+template <typename field_list, typename field>
+struct read_node {
+  using result_type = std::expected<field_list, std::string>;
+  using field_type = extract_type_from_field_v<field>;
+
+  // todo how to bundle buf with the res. a new box will be helpful?
+  std::ifstream buf;
+  read_node(std::ifstream& buf): buf(buf) {}
+
+  // todo: for const buf how to track the index in prefix sum
+  // auto operator()(result_type input) -> result_type {}
+};
+
+
+template <typename... T>
+void func(const T&... expected_list) {
+    (([&]() {
+        std::cout << typeid(T{}).name() << '\n';
+    }()), ...);
+}
 
 template <typename... fields>
-constexpr void struct_cast(struct_field_list<fields...>& field_list, const unsigned char* buffer) {
+constexpr auto struct_cast(const unsigned char* buffer)
+  -> std::expected<struct_field_list<fields...>, std::string>
+{
+  using field_list_type = struct_field_list<fields...>;
+  using res_type = std::expected<field_list_type, std::string>;
+
+  res_type res;
   std::size_t prefix_sum[sizeof...(fields) + 1] = {0};
   std::size_t index = 0;
 
-  ([&](auto& field) {
-    using field_type = std::decay_t<decltype(field)>;
-    prefix_sum[index + 1] = prefix_sum[index] + field_type::field_size;
-    if constexpr (is_struct_field_list_v<extract_type_from_field_v<field_type>>) {
-      struct_cast(field.value, buffer + prefix_sum[index]);
-    } else if constexpr (is_field_v<field_type>) {
-      field.read(reinterpret_cast<const char*>(buffer + prefix_sum[index]), field_type::field_size);
-    } else if constexpr (is_field_with_runtime_size_v<field_type>) {
-      field.read(reinterpret_cast<const char*>(buffer + prefix_sum[index]), field_list[field_type::field_accessor]);
-    }
-    ++index;
-  }(static_cast<fields&>(field_list)), ...);
+  // return (res | read_node<field_list_type, fields>{} | ...);
+  return (
+    res | 
+    [&](res_type input) -> res_type {
+      auto field = static_cast<fields&>(input);
+      using field_type = std::decay_t<decltype(field)>;
+      if constexpr (is_struct_field_list_v<extract_type_from_field_v<field_type>>) {
+        auto struct_read_res = struct_cast(field.value, buffer + prefix_sum[index]);
+      } else if constexpr (is_field_v<field_type>) {
+        field.value = read<field_type>(reinterpret_cast<const char*>(buffer + prefix_sum[index]), field_type::field_size);
+      } else if constexpr (is_field_with_runtime_size_v<field_type>) {
+        field.value = read<field_type>(reinterpret_cast<const char*>(buffer + prefix_sum[index]), res[field_type::field_accessor]);
+      }
+      // todo fix bug in case of updating the prefic sum for var len field
+      prefix_sum[index + 1] = prefix_sum[index] + field_type::field_size;
+      ++index;
+    } | 
+  ...);
 }
 
 template <typename... fields>
--- include/migration/field_reader.hpp
+++ include/migration/field_reader.hpp
@@ -14,7 +14,12 @@ struct variant_reader_impl;
 
 template <std::size_t idx, typename T>
 struct variant_reader_impl<idx, T, typelist::typelist<>, std::integer_sequence<std::size_t>> {
-  auto operator()([[maybe_unused]] std::size_t idx_r, const char* buf) 
+  auto operator()([[maybe_unused]] std::size_t idx_r, [[maybe_unused]]std::ifstream& ifs) 
+    -> std::expected<std::variant<T>, std::string> {
+    std::unexpected("Out of bounds");
+  }
+
+  auto operator()([[maybe_unused]] std::size_t idx_r, [[maybe_unused]]const char* buf) 
     -> std::expected<std::variant<T>, std::string> {
     std::unexpected("Out of bounds");
   }
@@ -29,6 +34,29 @@ struct variant_reader_impl<idx,
   using type_to_read = tl_front_t<typelist::typelist<types...>>;
   static constexpr std::size_t size_to_read = iseq_front_v<std::integer_sequence<std::size_t, sizes...>>;
 
+  auto operator()(std::size_t idx_r, std::ifstream& ifs) -> 
+    std::expected<std::variant<T>, std::string> {
+    if(idx_r == idx) {
+      if constexpr(is_struct_field_list_v<type_to_read>) {
+        auto res = struct_cast<front_t<types>>(buf);
+        return (res) ? *res : std::unexpected("error");
+      } else {
+        type_to_read obj;
+        if(!ifs.read(byte_addressof(obj), size_to_read))
+          return std::unexpected("buffer exhaustion");
+        return obj;
+      }
+    } else {
+      return 
+        variant_reader_impl<
+         idx + 1, 
+         T, 
+         tl_pop_t<typelist::typelist<types...>, 
+         iseq_pop_t<std::integer_sequence<std::size_t, sizes...>>
+       >(idx_r, buf)
+    }
+  }
+
   auto operator()(std::size_t idx_r, const char* buf) -> 
     std::expected<std::variant<T>, std::string> {
     if(idx_r == idx) {
@@ -36,15 +64,17 @@ struct variant_reader_impl<idx,
         auto res = struct_cast<front_t<types>>(buf);
         return (res) ? *res : std::unexpected("error");
       } else {
+        type_to_read obj;
         std::memcpy(to_void_ptr(obj), buffer, size_to_read);
       }
     } else {
-      return variant_reader_impl<
-               idx + 1, 
-               T, 
-               tl_pop_t<typelist::typelist<types...>, 
-               iseq_pop_t<std::integer_sequence<std::size_t, sizes...>>
-             >(idx_r, buf)
+      return 
+        variant_reader_impl<
+         idx + 1, 
+         T, 
+         tl_pop_t<typelist::typelist<types...>, 
+         iseq_pop_t<std::integer_sequence<std::size_t, sizes...>>
+       >(idx_r, buf)
     }
   }
 };
@@ -58,7 +88,10 @@ struct variant_reader<T, std::integer_sequence<std::size_t, sizes...>> {
   static constexpr auto size_list = std::integer_sequence<std::size_t, sizes...>{};
   using read_result = std::expected<T, std::string>;
 
-  template <std::size_t idx>
+  auto operator()(std::size_t idx_r, std::istream& ifs) -> read_result {
+    variant_reader_impl<0, T, types, size_list>(idx_r, ifs); 
+  }
+
   auto operator()(std::size_t idx_r, const char* buf) -> read_result {
     variant_reader_impl<0, T, types, size_list>(idx_r, buf); 
   }
