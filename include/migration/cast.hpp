7fbf9646ea4034281be933af96ce77ac358dff8c
hari <hariprasad12994@gmail.com>
2024-11-03 22:20:24 +0100

    Avoid providing fields as tpl_param for cast fn

--- include/migration/cast.hpp
+++ include/migration/cast.hpp
@@ -24,63 +24,70 @@ auto operator|(std::expected<expected_struct_field_list, error>& lhs, auto funct
 
 // todo constraint with struct field list else user might have to provide all
 // fields as params to the function
+template <typename T>
+struct struct_cast_impl;
+
 template <typename... fields>
-constexpr auto struct_cast(const unsigned char* buffer)
-  -> std::expected<struct_field_list<fields...>, std::string>
-{
+struct struct_cast_impl<struct_field_list<fields...>> {
   using field_list_type = struct_field_list<fields...>;
-  using res_type = std::expected<field_list_type, std::string>;
+  using res_type = std::expected<struct_field_list<fields...>, std::string>;
 
-  res_type res;
-  std::size_t prefix_sum[sizeof...(fields) + 1] = {0};
-  std::size_t index = 0;
+  constexpr auto operator()(const char* buffer) -> res_type {
+    res_type res;
+    std::size_t prefix_sum[sizeof...(fields) + 1] = {0};
+    std::size_t index = 0;
 
-  return (
-    res | 
-    ([&prefix_sum, &buffer, &index](field_list_type input) -> res_type {
-      using field_size = typename fields::field_size;
-      using field_type = typename fields::field_type;
+    return (
+      res | 
+      ([&prefix_sum, &buffer, &index](field_list_type input) -> res_type {
+        using field_size = typename fields::field_size;
+        using field_type = typename fields::field_type;
 
-      auto field = static_cast<fields&>(input);
-      std::expected<field_type, std::string> field_value;
-      auto buffer_pos = reinterpret_cast<const char*>(buffer + prefix_sum[index]);
+        auto field = static_cast<fields&>(input);
+        std::expected<field_type, std::string> field_value;
+        auto buffer_pos = reinterpret_cast<const char*>(buffer + prefix_sum[index]);
 
-      // todo variant field: static constexpr auto type_deduction_guide = type_deducer;
-      if constexpr (is_optional_field_v<field_type>) {
-        // todo impedence match variant and optional
-        // todo variable length optional field
-        field_value = field_type::field_presence_checker(input) ? 
-                      read<field_type>(buffer_pos, field_type::field_size) :
-                      res_type{std::nullopt{}};
-      } else if constexpr (is_union_field_v<field_type>) {
-        using variant_reader_type_t = variant_reader<field_type, field_size>;
-        auto type_index = field_type::type_deduction_guide(input); 
-        field_value = *type_index ? 
-                      variant_reader_type_t{}(type_index, buffer_pos): 
-                      type_index;
-      } else if constexpr (is_struct_field_list_v<extract_type_from_field_v<field_type>>) {
-        field_value = struct_cast(field.value, buffer_pos);
-      } else if constexpr (is_field_v<field_type>) {
-        field_value = read<field_type>(buffer_pos, field_type::field_size);
-      } else if constexpr (is_runtime_sized_field_v<field_type>) {
-        field_value = read<field_type>(buffer_pos, input[field_type::field_accessor]);
-      }
+        if constexpr (is_optional_field_v<field_type>) {
+          // todo variable length optional field
+          field_value = field_type::field_presence_checker(input) ? 
+                        read<field_type>(buffer_pos, field_type::field_size) :
+                        res_type{std::nullopt};
+        } else if constexpr (is_union_field_v<field_type>) {
+          using variant_reader_type_t = variant_reader<field_type, field_size>;
+          auto type_index = field_type::type_deduction_guide(input); 
+          field_value = *type_index ? 
+                        variant_reader_type_t{}(type_index, buffer_pos): 
+                        type_index;
+        } else if constexpr (is_struct_field_list_v<extract_type_from_field_v<field_type>>) {
+          field_value = struct_cast(field.value, buffer_pos);
+        } else if constexpr (is_field_v<field_type>) {
+          field_value = read<field_type>(buffer_pos, field_type::field_size);
+        } else if constexpr (is_runtime_sized_field_v<field_type>) {
+          field_value = read<field_type>(buffer_pos, input[field_type::field_accessor]);
+        }
 
-      // todo fix bug in case of updating the prefic sum for var len field
-      // also handle optional field, both read and unread case and variant read
-      prefix_sum[index + 1] = prefix_sum[index] + field_type::field_size;
-      ++index;
-      
-      // todo return std::unexpected to break the pipeline
-      // is this ok?
-      if(field_value) field.value = *field_value;
-      else input = field_value;
-      
-      // todo constraint checker
-      // static constexpr auto constraint_checker = constraint_on_value;
-      return input;
-    } | 
-  ...));
+        // todo fix bug in case of updating the prefic sum for var len field
+        // also handle optional field, both read and unread case and variant read
+        prefix_sum[index + 1] = prefix_sum[index] + field_type::field_size;
+        ++index;
+        
+        // todo return std::unexpected to break the pipeline
+        // is this ok?
+        if(field_value) field.value = *field_value;
+        else input = field_value;
+        
+        // todo constraint checker
+        // static constexpr auto constraint_checker = constraint_on_value;
+        return input;
+      } | 
+    ...));
+  }
+};
+
+template <typename T>
+  requires is_struct_field_list_v<T>
+constexpr auto struct_cast(const char* buffer) -> std::expected<T, std::string> {
+  return struct_cast_impl<T>{}(buffer);
 }
 
 template <typename... fields>
--- include/migration/field_size.hpp
+++ include/migration/field_size.hpp
@@ -2,6 +2,7 @@
 #define _FIELD_SIZE_HPP_
 
 #include "field_accessor.hpp"
+#include "bytes.hpp"
 
 
 template <typename size_type>
@@ -23,6 +24,9 @@ template <std::size_t... N>
 struct size_choices {
   static constexpr auto sizes = std::integer_sequence<std::size_t, N...>{};
 };
+
+template <typename T>
+struct runtime_size;
 
 template <typename field_accessor>
 struct runtime_size {
@@ -65,6 +69,22 @@ struct is_runtime_size<field_size<from_field<field_accessor>>> {
 template <typename T>
 inline constexpr bool is_runtime_size_v = is_runtime_size<T>::res;
 
+template <typename T>
+struct is_selectable_size;
+
+template <std::size_t... N>
+struct is_selectable_size<field_size<size_choices<N...>>> {
+  static constexpr bool res = true;
+};
+
+template <typename T>
+struct is_selectable_size {
+  static constexpr bool res = false;
+};
+
+template <typename T>
+inline constexpr bool is_selectable_size_v = is_comptime_size<T>::res;
+
 // Concepts for checking if a type is a size type
 template <typename T>
 concept comptime_size_like = is_comptime_size_v<T>;
@@ -72,6 +92,13 @@ concept comptime_size_like = is_comptime_size_v<T>;
 template <typename T>
 concept runtime_size_like = is_runtime_size_v<T>;
 
+template <typename T>
+concept selectable_size_like = is_selectable_size_v<T>;
+
+template <typename T>
+concept is_size_like = comptime_size_like<T> ||
+                       runtime_size_like<T> ||
+                       selectable_size_like<T>;
 
 namespace static_test {
 static_assert(is_runtime_size_v<field_size<from_field<"hello">>>);
