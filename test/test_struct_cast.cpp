7477fe266451e275f7e68e59c88fc2082bd6ac52
hari <hariprasad12994@gmail.com>
2024-11-12 20:55:58 +0100

    Restoring field validator test cases

--- test/test_struct_cast.cpp
+++ test/test_struct_cast.cpp
@@ -241,65 +241,65 @@ TEST_CASE("Test reading a meta_struct with array of records from binary file") {
 };
 
 
-// TEST_CASE("Test eq field constraint") {
-//   auto eq_obj = eq(42);
-//   REQUIRE(eq_obj(42) == true);
-//   REQUIRE(eq_obj(84) == false);
-// }
-//
-//
-// TEST_CASE("Test neq field constraint") {
-//   auto neq_obj = neq(42);
-//   REQUIRE(neq_obj(42) == false);
-//   REQUIRE(neq_obj(84) == true);
-// }
-//
-//
-// TEST_CASE("Test lt field constraint") {
-//   auto lt_obj = lt(42);
-//   REQUIRE(lt_obj(21) == true);
-//   REQUIRE(lt_obj(84) == false);
-// }
-//
-//
-// TEST_CASE("Test gt field constraint") {
-//   auto gt_obj = gt(42);
-//   REQUIRE(gt_obj(21) == false);
-//   REQUIRE(gt_obj(84) == true);
-// }
-//
-//
-// TEST_CASE("Test lte field constraint") {
-//   auto lte_obj = lte(42);
-//   REQUIRE(lte_obj(21) == true);
-//   REQUIRE(lte_obj(42) == true);
-//   REQUIRE(lte_obj(84) == false);
-// }
-//
-//
-// TEST_CASE("Test gte field constraint") {
-//   auto gte_obj = gte(42);
-//   REQUIRE(gte_obj(21) == false);
-//   REQUIRE(gte_obj(42) == true);
-//   REQUIRE(gte_obj(84) == true);
-// }
-//
-//
-// TEST_CASE("Test any_of field constraint") {
-//   auto any_of_obj = any_of(21, 42, 84);
-//   REQUIRE(any_of_obj(21) == true);
-//   REQUIRE(any_of_obj(42) == true);
-//   REQUIRE(any_of_obj(84) == true);
-//   REQUIRE(any_of_obj(100) == false);
-// }
-//
-//
-// TEST_CASE("Test open range interval check") {
-//   // constexpr auto ranges = std::array<range<u32>, 3>{range{2u, 4u}, range{6u, 9u}, range{13u, 15u}};
-//   // constexpr auto r = range{2u, 4u};
-// }
-//
-//
+TEST_CASE("Test eq field constraint") {
+  auto eq_obj = eq(42);
+  REQUIRE(eq_obj(42) == true);
+  REQUIRE(eq_obj(84) == false);
+}
+
+
+TEST_CASE("Test neq field constraint") {
+  auto neq_obj = neq(42);
+  REQUIRE(neq_obj(42) == false);
+  REQUIRE(neq_obj(84) == true);
+}
+
+
+TEST_CASE("Test lt field constraint") {
+  auto lt_obj = lt(42);
+  REQUIRE(lt_obj(21) == true);
+  REQUIRE(lt_obj(84) == false);
+}
+
+
+TEST_CASE("Test gt field constraint") {
+  auto gt_obj = gt(42);
+  REQUIRE(gt_obj(21) == false);
+  REQUIRE(gt_obj(84) == true);
+}
+
+
+TEST_CASE("Test lte field constraint") {
+  auto lte_obj = lte(42);
+  REQUIRE(lte_obj(21) == true);
+  REQUIRE(lte_obj(42) == true);
+  REQUIRE(lte_obj(84) == false);
+}
+
+
+TEST_CASE("Test gte field constraint") {
+  auto gte_obj = gte(42);
+  REQUIRE(gte_obj(21) == false);
+  REQUIRE(gte_obj(42) == true);
+  REQUIRE(gte_obj(84) == true);
+}
+
+
+TEST_CASE("Test any_of field constraint") {
+  auto any_of_obj = any_of(21, 42, 84);
+  REQUIRE(any_of_obj(21) == true);
+  REQUIRE(any_of_obj(42) == true);
+  REQUIRE(any_of_obj(84) == true);
+  REQUIRE(any_of_obj(100) == false);
+}
+
+
+TEST_CASE("Test open range interval check") {
+  // constexpr auto ranges = std::array<range<u32>, 3>{range{2u, 4u}, range{6u, 9u}, range{13u, 15u}};
+  // constexpr auto r = range{2u, 4u};
+}
+
+
 // TEST_CASE("Test magic number") {
 //   using test_struct_field_list = 
 //     struct_field_list<
