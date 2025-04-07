a65a5260a30cedd3885dd1c94fa3a79e6f335707
Hari Prasad Manoharan <hariprasad12994@gmail.com>
2025-04-07 23:16:56 +0200

    Testing spanstream read

--- test/test_foreign_read.cpp
+++ test/test_foreign_read.cpp
@@ -4,6 +4,8 @@
 #include <random>
 #include <vector>
 #include <fstream>
+#include <span>
+#include <spanstream>
 
 template <std::size_t size>
 constexpr bool has_builtin_bswap() {
@@ -14,9 +16,9 @@ constexpr bool has_builtin_bswap() {
     #else
       return false;
     #endif
-  
+  }
   return false;
-
+}
 
 
 template <typename T>
@@ -39,18 +41,18 @@ constexpr T swap_bytes(T value) {
     uint8_t* ptr = reinterpret_cast<uint8_t*>(&value_copy);
     std::reverse(ptr, ptr + size);
     return value_copy;
-  
-
+  }
+}
 
 template <typename T>
 char* byte_addressof(T& obj) {
   return reinterpret_cast<char*>(&obj);
-
+}
 
 template <typename T>
 char* byte_addressof(std::vector<T>& obj) {
   return reinterpret_cast<char*>(obj.data());
-
+}
 
 class bytewise_iterator {
   char* current;
@@ -62,20 +64,20 @@ public:
   using reference = char&;
 
   explicit constexpr bytewise_iterator(char* start)
-    : current(start) {
-  char& operator*() { return *current; 
-  bytewise_iterator& operator++() { ++current; return *this; 
+    : current(start) {}
+  char& operator*() { return *current; }
+  bytewise_iterator& operator++() { ++current; return *this; }
   bytewise_iterator operator++(int) {
     bytewise_iterator temp = *this;
     ++(*this);
     return temp;
-  
+  }
   bool operator!=(const bytewise_iterator& other) const {
     return current != other.current;
-  
+  }
   bool operator==(const bytewise_iterator& other) const {
     return current == other.current;
-  
+  }
 };
 
 class bytewise_reverse_iterator {
@@ -88,20 +90,20 @@ public:
   using reference = char&;
 
   explicit constexpr bytewise_reverse_iterator(char* start)
-    : current(start) {
-  char& operator*() { return *current; 
-  bytewise_reverse_iterator& operator++() { --current; return *this;  
+    : current(start) {}
+  char& operator*() { return *current; }
+  bytewise_reverse_iterator& operator++() { --current; return *this;  }
   bytewise_reverse_iterator operator++(int) {
     bytewise_reverse_iterator temp = *this;
     ++(*this);
     return temp;
-  
+  }
   bool operator!=(const bytewise_reverse_iterator& other) const {
     return current != other.current;
-  
+  }
   bool operator==(const bytewise_reverse_iterator& other) const {
     return current == other.current;
-  
+  }
 };
 
 class foreign_vector_iterator {
@@ -127,7 +129,7 @@ public:
     element_size(0),
     element_start(nullptr),
     element_end(nullptr),
-    current(nullptr) {
+    current(nullptr) {}
 
   foreign_vector_iterator(char* container_start, char* container_end, std::size_t element_size) 
     : container_start(container_start),
@@ -135,9 +137,9 @@ public:
       element_size(element_size),
       element_start(container_start + element_size - 1),
       element_end(container_start - 1),
-      current(element_start){
+      current(element_start){}
 
-  char& operator*() { return *current; 
+  char& operator*() { return *current; }
   foreign_vector_iterator& operator++() { 
     --current;
     if(current == element_end) {
@@ -147,20 +149,20 @@ public:
          current = nullptr;
       else 
         current = element_start;
-    
+    }
     return *this;
-  
+  }
   foreign_vector_iterator operator++(int) {
     foreign_vector_iterator temp = *this;
     ++(*this);
     return temp;
-  
+  }
   bool operator!=(const foreign_vector_iterator& other) const {
     return current != other.current;
-  
+  }
   bool operator==(const foreign_vector_iterator& other) const {
     return current == other.current;
-  
+  }
 };
 
 template <typename T>
@@ -173,12 +175,12 @@ struct raw_bytes {
 
 public:
   explicit constexpr raw_bytes(T& obj, std::size_t size)
-    : ptr_to_object(byte_addressof(obj)), size(size) {
+    : ptr_to_object(byte_addressof(obj)), size(size) {}
 
-  bytewise_iterator begin() { return bytewise_iterator(ptr_to_object); 
-  bytewise_iterator end() { return bytewise_iterator(ptr_to_object + size + 1); 
-  bytewise_reverse_iterator rbegin() { return bytewise_reverse_iterator(ptr_to_object + size - 1); 
-  bytewise_reverse_iterator rend() { return bytewise_reverse_iterator(ptr_to_object - 1); 
+  bytewise_iterator begin() { return bytewise_iterator(ptr_to_object); }
+  bytewise_iterator end() { return bytewise_iterator(ptr_to_object + size + 1); }
+  bytewise_reverse_iterator rbegin() { return bytewise_reverse_iterator(ptr_to_object + size - 1); }
+  bytewise_reverse_iterator rend() { return bytewise_reverse_iterator(ptr_to_object - 1); }
 };
 
 template <typename T>
@@ -189,16 +191,16 @@ struct raw_bytes<std::vector<T>> {
 
 public:
   explicit constexpr raw_bytes(std::vector<T>& obj)
-    : ptr_to_object(byte_addressof(obj)), vector_size(obj.size()), element_size(sizeof(std::vector<T>{}[0])) {
+    : ptr_to_object(byte_addressof(obj)), vector_size(obj.size()), element_size(sizeof(std::vector<T>{}[0])) {}
 
-  bytewise_iterator begin() { return bytewise_iterator(ptr_to_object); 
-  bytewise_iterator end() { return bytewise_iterator(ptr_to_object + vector_size * element_size); 
+  bytewise_iterator begin() { return bytewise_iterator(ptr_to_object); }
+  bytewise_iterator end() { return bytewise_iterator(ptr_to_object + vector_size * element_size); }
   foreign_vector_iterator fbegin() {
     return vector_size == 0 ? 
            foreign_vector_iterator() :
            foreign_vector_iterator(ptr_to_object, ptr_to_object + vector_size * element_size + 1, element_size);
-  
-  foreign_vector_iterator fend() { return foreign_vector_iterator(); 
+  }
+  foreign_vector_iterator fend() { return foreign_vector_iterator(); }
 };
 
 void create_binary_data(std::string file_name, std::size_t num_elements) {
@@ -210,12 +212,25 @@ void create_binary_data(std::string file_name, std::size_t num_elements) {
       auto temp = dist(gen);
       std::cout << std::hex << temp << " ";
       file.write(reinterpret_cast<const char*>(&temp), sizeof(temp));
-  
+  }
   std::cout << '\n';
   file.close();
+}
 
+void create_binary_vector(std::ospanstream& stream, std::size_t num_elements) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<uint32_t> dist(0, UINT32_MAX);
+  for (std::size_t i = 0; i < num_elements; ++i) {
+      auto temp = dist(gen);
+      std::cout << std::hex << temp << " ";
+      stream.write(reinterpret_cast<const char*>(&temp), sizeof(temp));
+  }
+  std::cout << '\n';
+}
 
 void bench_builtins_scalar_from_file() {
+    std::cout << __FUNCTION__ << '\n';
     constexpr std::size_t num_elements = 20;
     std::string file_name("test_builtins_scalar_from_file.bin");
     create_binary_data(file_name, num_elements);
@@ -226,12 +241,13 @@ void bench_builtins_scalar_from_file() {
     for (std::size_t idx = 0; idx < num_elements; ++idx) {
         stream.read(reinterpret_cast<char*>(&dest[idx]), sizeof(uint32_t));
         dest[idx] = swap_bytes(dest[idx]);
-    
-    for(auto val: dest) { std::cout << std::hex << val << " "; 
+    }
+    for(auto val: dest) { std::cout << std::hex << val << " "; }
     std::cout << '\n';
-
+}
 
 void bench_reverse_copy_scalar_from_file() {
+    std::cout << __FUNCTION__ << '\n';
     constexpr std::size_t num_elements = 20;
     std::string file_name("test_reverse_scalar_from_copy.bin");
     create_binary_data(file_name, num_elements);
@@ -242,13 +258,14 @@ void bench_reverse_copy_scalar_from_file() {
     for (std::size_t idx = 0; idx < num_elements; ++idx) {
       stream.read(reinterpret_cast<char*>(&dest[idx]), sizeof(uint32_t));
       std::reverse(reinterpret_cast<char*>(&dest[idx]), reinterpret_cast<char*>(&dest[idx]) + sizeof(uint32_t));
-    
-    for(auto val: dest) { std::cout << std::hex << val << " "; 
+    }
+    for(auto val: dest) { std::cout << std::hex << val << " "; }
     std::cout << '\n';
-
+}
 
 // Reads using foreign iterator
 void bench_custom_iterator_scalar_from_file() {
+    std::cout << __FUNCTION__ << '\n';
     constexpr std::size_t num_elements = 20;
     std::string file_name("test_custom_iterator_scalar_from_file.bin");
     create_binary_data(file_name, num_elements);
@@ -268,13 +285,14 @@ void bench_custom_iterator_scalar_from_file() {
         ++it;
         ++stream_it;
         b_count++;
-      
-    
-    for(auto val: dest) { std::cout << std::hex << val << " "; 
+      }
+    }
+    for(auto val: dest) { std::cout << std::hex << val << " "; }
     std::cout << '\n';
-
+}
 
 void bench_builtins_vector_from_file() {
+    std::cout << __FUNCTION__ << '\n';
     constexpr std::size_t num_elements = 20;
     std::string file_name("test_builtins_vector_from_file.bin");
     create_binary_data(file_name, num_elements);
@@ -285,12 +303,13 @@ void bench_builtins_vector_from_file() {
     for (std::size_t idx = 0; idx < num_elements; ++idx) {
         stream.read(reinterpret_cast<char*>(&dest[idx]), sizeof(uint32_t));
         dest[idx] = swap_bytes(dest[idx]);
-    
-    for(auto val: dest) { std::cout << std::hex << val << " "; 
+    }
+    for(auto val: dest) { std::cout << std::hex << val << " "; }
     std::cout << '\n';
-
+}
 
 void bench_reverse_copy_vector_from_file() {
+    std::cout << __FUNCTION__ << '\n';
     constexpr std::size_t num_elements = 20;
     std::string file_name("test_reverse_vector_from_copy.bin");
     create_binary_data(file_name, num_elements);
@@ -301,13 +320,14 @@ void bench_reverse_copy_vector_from_file() {
     for (std::size_t idx = 0; idx < num_elements; ++idx) {
       stream.read(reinterpret_cast<char*>(&dest[idx]), sizeof(uint32_t));
       std::reverse(reinterpret_cast<char*>(&dest[idx]), reinterpret_cast<char*>(&dest[idx]) + sizeof(uint32_t));
-    
-    for(auto val: dest) { std::cout << std::hex << val << " "; 
+    }
+    for(auto val: dest) { std::cout << std::hex << val << " "; }
     std::cout << '\n';
-
+}
 
 // Reads using foreign iterator
 void bench_custom_iterator_vector_from_file() {
+    std::cout << __FUNCTION__ << '\n';
     constexpr std::size_t num_elements = 20;
     std::string file_name("test_custom_iterator_vector_from_file.bin");
     create_binary_data(file_name, num_elements);
@@ -324,10 +344,28 @@ void bench_custom_iterator_vector_from_file() {
       *it = *stream_it;
       ++it;
       ++stream_it;
-    
-    for(auto val: dest) { std::cout << std::hex << val << " "; 
+    }
+    for(auto val: dest) { std::cout << std::hex << val << " "; }
     std::cout << '\n';
+}
 
+void bench_builtins_scalar_from_vector() {
+    std::cout << __FUNCTION__ << '\n';
+    constexpr std::size_t num_elements = 20;
+    std::vector<char> buffer(num_elements * sizeof(uint32_t));
+    std::ospanstream out_stream{std::span(buffer)};
+    create_binary_vector(out_stream, num_elements);
+    std::vector<uint32_t> dest;
+    dest.resize(num_elements);
+    std::ispanstream in_stream{std::span(buffer)};
+
+    for (std::size_t idx = 0; idx < num_elements; ++idx) {
+        in_stream.read(reinterpret_cast<char*>(&dest[idx]), sizeof(uint32_t));
+        dest[idx] = swap_bytes(dest[idx]);
+    }
+    for(auto val: dest) { std::cout << std::hex << val << " "; }
+    std::cout << '\n';
+}
 
 auto main(void) -> int {
     bench_builtins_scalar_from_file();
@@ -336,5 +374,6 @@ auto main(void) -> int {
     bench_builtins_vector_from_file();
     bench_reverse_copy_vector_from_file();
     bench_custom_iterator_vector_from_file();
+    bench_builtins_scalar_from_vector();
     return 0;
-
+}
