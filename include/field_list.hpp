f501ec7fc9a3e368157bb0618040564c334b93b4
Hari Prasad Manoharan <hariprasad12994@gmail.com>
2025-04-23 22:55:20 +0200

    Adding size resolution checks for union types

--- include/field_list.hpp
+++ include/field_list.hpp
@@ -36,12 +36,12 @@ concept all_field_like = (field_like<fields> && ...);
 
  
 template <typename flist, typename glist>
-struct size_indices_resolved;
+struct size_dependencies_resolved;
  
 // if the second list is empty, we have processed all the fields without short circuiting
 // so we can treat that we have checked all the fields and all checks have passed
 template <typename... xs>
-struct size_indices_resolved<
+struct size_dependencies_resolved<
   typelist::list<xs...>, 
   typelist::list<>
 > 
@@ -52,7 +52,7 @@ struct size_indices_resolved<
 
 // if the first field has the size dependant on a field, then its size cannot be deduced
 template <fixed_string id, typename T, variable_size_like size, auto constraint, typename... rest>
-struct size_indices_resolved<
+struct size_dependencies_resolved<
   typelist::list<>, 
   typelist::list<field<id, T, size, constraint>, rest...>
 > 
@@ -62,14 +62,14 @@ struct size_indices_resolved<
 
 template <fixed_string id, typename T, fixed_size_like size, auto constraint, 
           typename... xs, typename... rest>
-struct size_indices_resolved<
+struct size_dependencies_resolved<
   typelist::list<xs...>, 
   typelist::list<field<id, T, size, constraint>, rest...>
 > 
 {
   static constexpr bool is_resolved =
     true &&
-    size_indices_resolved<
+    size_dependencies_resolved<
       typelist::list<xs..., field<id, T, size, constraint>>, 
       typelist::list<rest...>
     >::is_resolved;
@@ -77,14 +77,14 @@ struct size_indices_resolved<
 
 template <fixed_string id, typename T, size_dont_care_like size, auto constraint, 
           typename... xs, typename... rest>
-struct size_indices_resolved<
+struct size_dependencies_resolved<
   typelist::list<xs...>, 
   typelist::list<field<id, T, size, constraint>, rest...>
 > 
 {
   static constexpr bool is_resolved =
     true &&
-    size_indices_resolved<
+    size_dependencies_resolved<
       typelist::list<xs..., field<id, T, size, constraint>>, 
       typelist::list<rest...>
     >::is_resolved;
@@ -92,7 +92,7 @@ struct size_indices_resolved<
 
 template <fixed_string id, typename T, fixed_string len_source, auto constraint, 
           typename x, typename... xs, typename... rest>
-struct size_indices_resolved<
+struct size_dependencies_resolved<
   typelist::list<x, xs...>, 
   typelist::list<field<id, T, field_size<len_from_field<len_source>>, constraint>, rest...>
 > 
@@ -102,7 +102,7 @@ struct size_indices_resolved<
     !std::is_same_v<f, field_lookup_failed> &&
     // todo ensure that the field type is integral
     // std::is_same_v<extract_type_from_field_v<f>, std::size_t> &&
-    size_indices_resolved<
+    size_dependencies_resolved<
       typelist::list<x, xs..., field<id, T, field_size<len_from_field<len_source>>, constraint>>, 
       typelist::list<rest...>
     >::is_resolved;
@@ -118,75 +118,72 @@ struct bulk_look_up<typelist::list<fields...>, fixed_string_list<field_names...>
   static constexpr bool res = !typelist::any_of_v<typelist::list<field_lookup_v<field_list, field_names>...>, field_lookup_failed>;
 };
 
+template <typename... fields, typename... field_needles>
+struct bulk_look_up<typelist::list<fields...>, field_choice_list<field_needles...>> {
+  using haystack = typelist::list<fields...>;
+  static constexpr bool res = (... && (size_dependencies_resolved<haystack, typelist::list<field_needles>>::is_resolved));
+};
 
 template <fixed_string id, typename T, auto callable, field_name_list req_fields, auto constraint, 
           typename x, typename... xs, typename... rest>
-struct size_indices_resolved<
+struct size_dependencies_resolved<
   typelist::list<x, xs...>, 
   typelist::list<field<id, T, field_size<len_from_fields<callable, req_fields>>, constraint>, rest...>
 > 
 {
-  // todo enable actual checking such each req_field occurs left to current field
   static constexpr bool is_resolved =
     bulk_look_up<typelist::list<x, xs...>, req_fields>::res &&
-    size_indices_resolved<
+    size_dependencies_resolved<
       typelist::list<x, xs..., field<id, T, field_size<len_from_fields<callable, req_fields>>, constraint>>, 
       typelist::list<rest...>
     >::is_resolved;
-
-  // using f = field_lookup_v<typelist::list<x, xs...>, len_source>;
-  // static constexpr bool is_resolved =
-  //   !std::is_same_v<f, field_lookup_failed> &&
-  //   // todo ensure that the field type is integral
-  //   // std::is_same_v<extract_type_from_field_v<f>, std::size_t> &&
-  //   size_indices_resolved<
-  //     typelist::list<x, xs..., field<id, T, field_size<len_from_fields<callable, req_fields>>, constraint>>, 
-  //     typelist::list<rest...>
-  //   >::is_resolved;
 };
 
 template <fixed_string id, typename T, typename size, auto constraint, 
           typename present_only_if, typename optional,
           typename... xs, typename... rest>
-struct size_indices_resolved<
+struct size_dependencies_resolved<
   typelist::list<xs...>, 
   typelist::list<maybe_field<field<id, T, size, constraint>, present_only_if, optional>, rest...>
 > 
 {
+  // we create a dummy field and wrap it in a list to reuse size_indices_resolved
   static constexpr bool is_resolved =
-    size_indices_resolved<typelist::list<xs...>, typelist::list<field<id, T, size, constraint>>>::is_resolved &&
-    size_indices_resolved<
+    size_dependencies_resolved<typelist::list<xs...>, typelist::list<field<id, T, size, constraint>>>::is_resolved &&
+    size_dependencies_resolved<
       typelist::list<xs..., maybe_field<field<id, T, size, constraint>, present_only_if, optional>>, 
       typelist::list<rest...>
     >::is_resolved;
 };
 
-template <fixed_string id, typename type_deducer, typename type, typename size_type,
+template <fixed_string id, typename type_deducer, typename type,
           auto constraint_on_value, typename variant, typename field_choices_t,
-          typename... xs, typename... rest>
-struct size_indices_resolved<
+          typename size, typename... xs, typename... rest>
+struct size_dependencies_resolved<
   typelist::list<xs...>, 
-  typelist::list<union_field<id, type_deducer, type, size_type, constraint_on_value, variant, field_choices_t>, rest...>
+  typelist::list<union_field<id, type_deducer, type, size, constraint_on_value, variant, field_choices_t>, rest...>
 > 
 {
+  // using dummy_field_list = typelist::list<field<id, dummy, choices, constraint_on_value>...>;
   // todo check resolution for each size in the size_type
   static constexpr bool is_resolved =
-    true &&
-    size_indices_resolved<
-      typelist::list<xs..., union_field<id, type_deducer, type, size_type, constraint_on_value, variant, field_choices_t>>, 
+    bulk_look_up<typelist::list<xs...>, field_choices_t>::res &&
+    // true &&
+    size_dependencies_resolved<
+      typelist::list<xs..., union_field<id, type_deducer, type, size, constraint_on_value, variant, field_choices_t>>, 
       typelist::list<rest...>
     >::is_resolved;
 };
 
 template <typename list>
-inline constexpr bool size_indices_resolved_v = size_indices_resolved<typelist::list<>, list>::is_resolved;
+inline constexpr bool size_dependencies_resolved_v = size_dependencies_resolved<typelist::list<>, list>::is_resolved;
 
 
 // todo: impl dependencies resolution
 template <typename... fields>
   requires all_field_like<fields...> &&
            has_unique_field_ids_v<fields::field_id...> &&
-           size_indices_resolved_v<typelist::list<fields...>>
+           size_dependencies_resolved_v<typelist::list<fields...>>
 struct struct_field_list : struct_field_list_base, fields... {
   struct_field_list() = default;
   template <typename field_accessor, 
